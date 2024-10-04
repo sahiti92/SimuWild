6c92d9358c17c95ef28c454ca36072741cc1e9b4
Greeshma <cs22b044@iittp.ac.in>
2024-10-05 00:24:33 +0530

    Made basic template for dashboard

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Dashboard />
+  </StrictMode>
+);
