907b28d6c11fecc044be4887269681dfc39e037d
Snehitha <psnehitha3333@gmail.com>
2024-10-05 12:04:39 +0530

    modi

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
+import Scenario2 from "./Components/ScenarioPlay/Scenario2.jsx";
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Scenario2/>
   </StrictMode>,
 )
