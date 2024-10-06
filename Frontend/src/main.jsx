93c0d0143d1f12bbecb526650e17ce1a7bb1d36a
Greeshma <cs22b044@iittp.ac.in>
2024-10-06 23:55:54 +0530

    Dashboard added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-import Dashboard from "./Components/Dashboard/Dashboard.jsx";
+//import "./index.css";
+import Dashboard2 from "./Components/DashBoard2/Dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Dashboard />
+    <Dashboard2 />
   </StrictMode>
 );
