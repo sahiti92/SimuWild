67468166aac8d75214a4c9fc49dc68c3a9d1fec4
Sahiti Vempalli <vsahiti92005@gmail.com>
2024-10-06 21:32:57 +0530

    Backend data preprocessed and added

--- Frontend/src/Components/WatchList/WatchList.jsx
+++ Frontend/src/Components/WatchList/WatchList.jsx
@@ -1,25 +1,46 @@
-import React from "react";
-import "./Watchlist.css"; // Import the CSS file
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './Watchlist.css';
 
 const Watchlist = () => {
-  const species = [
-    { img: "/assam_roofed_turtle.jpg", name: "Assam roofed turtle" },
-    { img: "/Muskdeer.jpg" },
-    { img: "/Clouded_leopard.jpg" },
-    { img: "/Elephant.jpg" },
-    { img: "/Great-Indian-one-horned-rhinoceros-India.jpg" },
-    { img: "/Humpback_Whale.jpg" },
+  const images = [
+    { src: '/assam_roofed_turtle.jpg', alt: 'Assam Roofed Turtle', title: 'Assam Roofed Turtle' },
+    { src: '/Clouded_leopard.jpg', alt: 'Clouded Leopard', title: 'Clouded Leopard' },
+    { src: '/Elephant.jpg', alt: 'Indian Elephant', title: 'Indian Elephant' },
+    { src: '/Great-Indian-one-horned-rhinoceros-India.jpg', alt: 'Indian Rhinoceros', title: 'Indian Rhinoceros' },
   ];
 
   return (
-    <div className="grid-container">
-      {species.map((specie, index) => (
-        <div key={index} className="grid-item">
-          <img src={specie.img} alt={specie.name} />
-          <div className="species-name">{specie.name}</div>
-        </div>
+    <Swiper
+      modules={[Navigation, Pagination]}
+      spaceBetween={-30} // Negative space to make slides overlap
+      slidesPerView={2.2} // Show part of the next and previous slides
+      centeredSlides={true}
+      loop={true}
+      navigation={true}
+      pagination={{ clickable: true }}
+      grabCursor={true}
+      breakpoints={{
+        768: {
+          slidesPerView: 1.5, // Adjust for mobile view
+        },
+      }}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <div className="card">
+            <img src={image.src} alt={image.alt} />
+            <div className="overlay">
+              <div className="text">{image.title}</div>
+            </div>
+          </div>
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 };
 
