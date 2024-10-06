77cb6ec5e8bedb934caf6fd1fe14256294ddde41
saipreethika12 <saipreethika1207@gmail.com>
2024-10-06 22:46:45 +0530

    update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -26,3 +26,25 @@ createRoot(document.getElementById("root")).render(
     </Router>
   </StrictMode>
 );
+
+// import App from "./App.jsx";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { store } from "./redux/store/store.js";
+// import { Provider } from "react-redux";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import "./index.css";
+
+// const client = new QueryClient();
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <QueryClientProvider client={client}>
+//         <App />
+//         <ReactQueryDevtools initialIsOpen={false} />
+//       </QueryClientProvider>
+//     </Provider>
+//   </React.StrictMode>
+// );
