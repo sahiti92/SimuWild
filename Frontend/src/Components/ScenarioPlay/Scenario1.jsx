9398a403cdbbfe563263543007eab8821277588b
Snehitha <psnehitha3333@gmail.com>
2024-10-07 17:03:49 +0530

    play button added

--- Frontend/src/Components/ScenarioPlay/Scenario1.jsx
+++ Frontend/src/Components/ScenarioPlay/Scenario1.jsx
@@ -16,6 +16,7 @@ const Scenario1 = () => {
           In Assam's Sonitpur district, the destruction of natural habitats due to deforestation has led elephants to forage in human settlements, resulting in significant damage to crops and homes. In 2020, elephants in this region trampled through villages and farms, seeking food, leading to the destruction of over 200 hectares of crops and the death of both humans and elephants. The expansion of tea plantations and infrastructure projects has fragmented the elephant corridors, cutting off traditional migration paths, forcing elephants into villages and farmlands. In retaliation, locals resort to violent measures, including electric fences, which lead to more elephant deaths.
 
           </p>
+          <button className="small-play-button">Play</button>
         </div>
       </div>
       <button className="small-play-button">Play</button>
